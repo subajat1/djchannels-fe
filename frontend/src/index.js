154bb0722d6412b32cb4a7f21f648ab4846e10dc
subajat1 <bayumunajat@outlook.com>
2018-12-30 17:41:20 +0800

    update fe

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,20 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Chat from './containers/Chat';
-import WebSocketInstance from './websocket';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, compose, applyMiddleware, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import "antd/dist/antd.css";
+import authReducer from "./store/reducers/auth";
+import navReducer from "./store/reducers/nav";
+import messageReducer from "./store/reducers/message";
+import App from "./App";
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-class App extends React.Component {
+function configureStore() {
+  const rootReducer = combineReducers({
+    auth: authReducer,
+    nav: navReducer,
+    message: messageReducer
+  });
 
-    componentDidMount() {
-        WebSocketInstance.connect();
-    }
+  const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk))
+  );
 
-    render(){
-        return (
-            <Chat />
-        )
-    }
+  //   if (module.hot) {
+  //     module.hot.accept("./store/reducers", () => {
+  //       const nextRootReducer = require("./store/reducers/auth");
+  //       store.replaceReducer(nextRootReducer);
+  //     });
+  //   }
+
+  return store;
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const store = configureStore();
+
+const app = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(app, document.getElementById("app"));
